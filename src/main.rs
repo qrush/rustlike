d5074f8dfe302fabe08fe377897fa344b2dab051
Nick Quaranto <nick@quaran.to>
2017-06-05 22:55:10 +0200

    Moving around

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate tcod;
 
 use tcod::console::*;
 use tcod::colors;
+use tcod::input::Key;
+use tcod::input::KeyCode::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -17,10 +19,37 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !root.window_closed() {
         root.set_default_foreground(colors::WHITE);
-        root.put_char(10, 10, '@', BackgroundFlag::None);
         root.flush();
-        root.wait_for_keypress(true);
+
+        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+
+        if exit {
+            break
+        }
     }
+}
+
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    let key = root.wait_for_keypress(true);
+
+    match key {
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true,
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        _ => {},
+    }
+    false
 }
